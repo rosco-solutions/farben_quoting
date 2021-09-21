cf525f85a38da853adee80e2259722c9d9bef70b
rosco-solutions <kw@rosco.solutions>
2021-09-21 21:03:26 +1000

    update output format. added heading fields

--- farben_quoting/farben_quoting/doctype/quotes/quotes.js
+++ farben_quoting/farben_quoting/doctype/quotes/quotes.js
@@ -44,16 +44,108 @@ frappe.ui.form.on('Quotes', {
                     }
                 })
             });
-    }
+    },
+	labour(frm){
+		calc_costing(frm);
+		// frm.doc.price = frm.doc.labour + frm.doc.products + frm.doc.equipment + frm.doc.scaffolding;
+		// frm.refresh_field('price');
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	products(frm){
+		calc_costing(frm);
+		// frm.doc.price = frm.doc.labour + frm.doc.products + frm.doc.equipment + frm.doc.scaffolding;
+		// frm.refresh_field('price');
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	equipment(frm){
+		calc_costing(frm);
+		// frm.doc.price = frm.doc.labour + frm.doc.products + frm.doc.equipment + frm.doc.scaffolding;
+		// frm.refresh_field('price');
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	scaffolding(frm){
+		calc_costing(frm);
+		// frm.doc.price = frm.doc.labour + frm.doc.products + frm.doc.equipment + frm.doc.scaffolding;
+		// frm.refresh_field('price');
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	labour_gst(frm){
+		calc_costing_gst(frm);
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	products_gst(frm){
+		calc_costing_gst(frm);
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	equipment_gst(frm){
+		calc_costing_gst(frm);
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
+	scaffolding_gst(frm){
+		calc_costing_gst(frm);
+		// frm.doc.gst = 0.1 * cint(frm.doc.labour_gst) * frm.doc.labour + 0.1 * cint(frm.doc.products_gst) * frm.doc.products + 
+		// 				0.1 * cint(frm.doc.equipment_gst) * frm.doc.equipment + 0.1 * cint(frm.doc.scaffolding_gst) * frm.doc.scaffolding;
+		// frm.refresh_field('gst');
+		// frm.doc.total_price = frm.doc.price + frm.doc.gst;
+		// frm.refresh_field('total_price');
+	},
 });
+
+function calc_costing(myfrm){
+	myfrm.doc.price = myfrm.doc.labour + myfrm.doc.products + myfrm.doc.equipment + myfrm.doc.scaffolding;
+	myfrm.refresh_field('price');
+	myfrm.doc.gst = 0.1 * cint(myfrm.doc.labour_gst) * myfrm.doc.labour + 0.1 * cint(myfrm.doc.products_gst) * myfrm.doc.products + 
+					0.1 * cint(myfrm.doc.equipment_gst) * myfrm.doc.equipment + 0.1 * cint(myfrm.doc.scaffolding_gst) * myfrm.doc.scaffolding;
+	myfrm.refresh_field('gst');
+	myfrm.doc.total_price = myfrm.doc.price + myfrm.doc.gst;
+	myfrm.refresh_field('total_price');
+}
+
+function calc_costing_gst(myfrm){
+	myfrm.doc.gst = 0.1 * cint(myfrm.doc.labour_gst) * myfrm.doc.labour + 0.1 * cint(myfrm.doc.products_gst) * myfrm.doc.products + 
+					0.1 * cint(myfrm.doc.equipment_gst) * myfrm.doc.equipment + 0.1 * cint(myfrm.doc.scaffolding_gst) * myfrm.doc.scaffolding;
+	myfrm.refresh_field('gst');
+	myfrm.doc.total_price = myfrm.doc.price + myfrm.doc.gst;
+	myfrm.refresh_field('total_price');
+}
 
 frappe.ui.form.on('Quote Details', {
 	paragraph_name(frm, cdt, cdn) {
 		var child = locals[cdt][cdn];
-		frappe.db.get_value("Standard Paragraphs", {"paragraph_name": child.paragraph_name}, "paragraph_content", function(value) {
-            child.content = value.paragraph_content;
+		frappe.db.get_value("Standard Paragraphs", {"paragraph_name": child.paragraph_name}, ["paragraph_heading", "paragraph_content"], function(value) {
+            child.heading = value.paragraph_heading;
+			child.content = value.paragraph_content;
 			frm.refresh_field('quote_details_start');
 			frm.refresh_field('quote_details_end');
+			frm.refresh_field('final_notes');
         });
 	}
 });
