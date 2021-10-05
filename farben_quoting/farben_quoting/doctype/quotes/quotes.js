c11ce7e6e6263d319c5ba102938b3e371e1ecb76
rosco-solutions <kw@rosco.solutions>
2021-10-05 18:46:20 +1000

    formating of quote output, page break added

--- farben_quoting/farben_quoting/doctype/quotes/quotes.js
+++ farben_quoting/farben_quoting/doctype/quotes/quotes.js
@@ -1,18 +1,16 @@
 // Copyright (c) 2021, Rosco Solutions and contributors
 // For license information, please see license.txt
 
+var mytimer;
 frappe.ui.form.on('Quotes', {
 	onload(frm) {
-        frm.set_value('customer', '')
-            .then(function(){
-                frm.set_query('customer', function(){
-                    return {
-                        'filters': {
-                            'company': frm.doc.company
-                        }
-                    }
-                })
-            });
+		frm.set_query('customer', function(){
+			return {
+				'filters': {
+					'company': frm.doc.company
+				}
+			}
+		});
 	},
 	job_type(frm){
 		frappe.confirm(
@@ -63,33 +61,66 @@ frappe.ui.form.on('Quotes', {
 										frm.refresh_field(mySectionFieldName);
 										loop_defaults(i+1);
 								})
-							}	
+							} else if (mySection == 'Paints Included') {
+								frappe.db.get_value(myDefaults[i].print_wording_type, myDefaults[i].default_entry, ['name'])
+									.then(p => {
+										let row = frm.add_child(mySectionFieldName, {
+											paint: p.message.name
+										});
+										frm.refresh_field(mySectionFieldName);
+										loop_defaults(i+1);
+								})
+							} else if (mySection == 'Internal Colours Included') {
+								frappe.db.get_value(myDefaults[i].print_wording_type, myDefaults[i].default_entry, ['name'])
+									.then(p => {
+										let row = frm.add_child(mySectionFieldName, {
+											internal_colour_included: p.message.name
+										});
+										frm.refresh_field(mySectionFieldName);
+										loop_defaults(i+1);
+								})
+							} else if (mySection == 'External Colours Included') {
+								frappe.db.get_value(myDefaults[i].print_wording_type, myDefaults[i].default_entry, ['name'])
+									.then(p => {
+										let row = frm.add_child(mySectionFieldName, {
+											external_colour_included: p.message.name
+										});
+										frm.refresh_field(mySectionFieldName);
+										loop_defaults(i+1);
+								})
+							}
 						})(0);
 					})
-			},
-			function(){
-				msgprint('you selected no');
-			}
+			}//,
+			// function(){
+				// msgprint('you selected no');
+			// }
 		)
 	},
 	customer(frm){
-		frappe.db.get_value("Customers", {"name": frm.doc.customer}, ["address_line_1", "address_line_2", "csp", "phone"], function(value) {
-            frm.doc.address_line_1 = value.address_line_1;
-			frm.doc.address_line_2 = value.address_line_2;
-			frm.doc.csp = value.csp;
-			frm.doc.phone = value.phone;
-            frm.doc.job_address_line_1 = value.address_line_1;
-			frm.doc.job_address_line_2 = value.address_line_2;
-			frm.doc.job_csp = value.csp;
-			frm.refresh_field('address_line_1');
-			frm.refresh_field('address_line_2');
-			frm.refresh_field('csp');
-			frm.refresh_field('phone');
-			frm.refresh_field('job_address_line_1');
-			frm.refresh_field('job_address_line_2');
-			frm.refresh_field('job_csp');
-			
-        });
+		if (frm.doc.customer){
+			frappe.db.get_value("Customers", {"name": frm.doc.customer}, ["address_line_1", "address_line_2", "csp", "phone", "email"], function(value) {
+				frm.doc.address_line_1 = value.address_line_1;
+				frm.doc.address_line_2 = value.address_line_2;
+				frm.doc.csp = value.csp;
+				frm.doc.phone = value.phone;
+				frm.doc.job_address_line_1 = value.address_line_1;
+				frm.doc.job_address_line_2 = value.address_line_2;
+				frm.doc.job_csp = value.csp;
+				frm.doc.email = value.email;
+				frm.refresh_field('address_line_1');
+				frm.refresh_field('address_line_2');
+				frm.refresh_field('csp');
+				frm.refresh_field('phone');
+				frm.refresh_field('job_address_line_1');
+				frm.refresh_field('job_address_line_2');
+				frm.refresh_field('job_csp');
+				frm.refresh_field('email');
+			});
+		} else {
+			if (mytimer) clearTimeout(mytimer);
+			mytimer = setTimeout(clear_address_fields,50,frm);
+		}
 	},
 	company(frm){
         frm.set_value('customer', '')
@@ -147,25 +178,48 @@ function calc_costing_gst(myfrm){
 	myfrm.refresh_field('total_price');
 }
 
+function clear_address_fields(myfrm){
+	myfrm.doc.address_line_1 = '';
+	myfrm.doc.address_line_2 = '';
+	myfrm.doc.csp = '';
+	myfrm.doc.phone = '';
+	myfrm.doc.job_address_line_1 = '';
+	myfrm.doc.job_address_line_2 = '';
+	myfrm.doc.job_csp = '';
+	myfrm.doc.email = '';
+	myfrm.refresh_field('address_line_1');
+	myfrm.refresh_field('address_line_2');
+	myfrm.refresh_field('csp');
+	myfrm.refresh_field('phone');
+	myfrm.refresh_field('job_address_line_1');
+	myfrm.refresh_field('job_address_line_2');
+	myfrm.refresh_field('job_csp');
+	myfrm.refresh_field('email');
+}
+
 frappe.ui.form.on('Quote Details', {
 	paragraph_name(frm, cdt, cdn) {
 		var child = locals[cdt][cdn];
-		frappe.db.get_value("Standard Paragraphs", {"paragraph_name": child.paragraph_name}, ["paragraph_heading", "paragraph_content"], function(value) {
-            child.heading = value.paragraph_heading;
-			child.content = value.paragraph_content;
-			frm.refresh_field('quote_details_start');
-			frm.refresh_field('quote_details_end');
-			frm.refresh_field('final_notes');
-        });
+		if (child.paragraph_name) {
+			frappe.db.get_value("Standard Paragraphs", {"paragraph_name": child.paragraph_name}, ["paragraph_heading", "paragraph_content"], function(value) {
+				child.heading = value.paragraph_heading;
+				child.content = value.paragraph_content;
+				frm.refresh_field('quote_details_start');
+				frm.refresh_field('quote_details_end');
+				frm.refresh_field('final_notes');
+			});
+		};
 	}
 });
 
 frappe.ui.form.on('Quote Works Included', {
 	work_type(frm, cdt, cdn) {
 		var child = locals[cdt][cdn];
-		frappe.db.get_value("Works Included", {"work_type": child.work_type}, "work_details", function(value) {
-            child.work_details = value.work_details;
-			frm.refresh_field('works_included');
-        });
+		if (child.work_type) {
+			frappe.db.get_value("Works Included", {"work_type": child.work_type}, "work_details", function(value) {
+				child.work_details = value.work_details;
+				frm.refresh_field('works_included');
+			});
+		};
 	}
 });
